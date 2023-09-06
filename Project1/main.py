2d47810128d0f0af5d5e447dea381be692eea4d8
Theo <koppenth@hotmail.de>
2023-09-06 13:40:08 +0200

    Finally running

--- Project1/main.py
+++ Project1/main.py
@@ -1,8 +1,8 @@
 import csv
-#import tikzplotlib
+import tikzplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-#import random as rd
+import random as rd
 
 # good programming practice in python
 # - avoid loops (vectorise, use numpy, stencils)
@@ -18,7 +18,7 @@ class Potts:
     Implementation of the Potts model
     """
     # Carmen: class structure
-    def __init__(self, L=2, T=0.002, q=2, J=1):
+    def __init__(self, L=2, T=1, q=2, J=1):
         # parameters
         self.L = L #number of lattice sites per side
         self.N = L * L #TOTAL number of lattice sites
@@ -33,53 +33,42 @@ class Potts:
         # Anna
         T = self.T
         L = self.L
-        s = self.s
-        Q = self.Q
         # steps 1-3 p.12
 
         # step 1: choose random spin
         # pick random coordinates
         rng = np.random.default_rng()
-        cx = rng.choice(len(s[0]))      ## UNIFORMLY CHOOSEN? (I would say yes)
-        cy = rng.choice(len(s[:,0]))
+        cx = rng.choice(self.s.shape[0])      ## UNIFORMLY CHOOSEN? (I would say yes)
+        cy = rng.choice(self.s.shape[1])
         c = [cx,cy]
 
         # step 2: propose state and calculate enrgy change
-        s_new = rng.choice(Q) 
-        s_old = s[tuple(c)]
+        s_new = 1+rng.choice(self.q)
+        s_old = self.s[tuple(c)]
 
         print('s_new:', s_new, 's_old:', s_old)
         neighbours = np.array([[0,1],[1,0],[-1,0],[0,-1]])
         neighbours = np.mod(c+neighbours, np.array([L,L]))
         print('c:',c ,'neighbours:', neighbours)
-        s_neighbours = s[tuple(map(tuple,neighbours.T))]
+        s_neighbours = self.s[tuple(map(tuple,neighbours.T))]
         print('s_neighb:', s_neighbours)
 
         delta_E = -self.J*(np.sum(s_new == s_neighbours)-np.sum(s_old == s_neighbours))         # What is the sum doing?
         print('delta_E:', delta_E)
 
-        # Acceopt or deny change 
-        if delta_E <= 0:
-            s[tuple(c)] = s_new
-        elif rd.random() < np.exp(-delta_E/ T):
-            s[tuple(c)] = s_new  
-        
-        return s
-
-    def nearest_neighbours():
-        # returns a stencil given a coordinate
-        pass
+        # Accept or deny change 
+        if rd.random() < np.exp(-delta_E/ T):
+            self.s[tuple(c)] = s_new
 
     def run_simulation(self):
 
         # initialise s
         s = self.s
-        while i < self.M:
-            s = Potts.MC_step()
-            print(s)
-            i+=1
-        
-        pass
+        for i in range(self.M):
+            self.MC_step()
+            print(self.s)
+            if i % 10 == 0:
+                self.plot_state()
 
     def get_E(self):
         # Carmen
@@ -100,8 +89,8 @@ class Potts:
         ax.imshow(self.s, cmap='Set1')
         ax.set(xlabel=r'x', ylabel=r'y')
 
-       # if filename:
-       #     tikzplotlib.save(filename)
+        if filename:
+            tikzplotlib.save(filename)
 
         if show_plt:
             plt.show()
@@ -124,6 +113,6 @@ if __name__ == '__main__':
 
     if 1:
         # Test the function MC_step
-        model = Potts(4)
-        model.MC_step()
+        model = Potts(10)
+        model.run_simulation()
         print(model.s)
