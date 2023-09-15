fb1ed72c84b6e00740183df7575b5535e0f5aede
Theo <koppenth@hotmail.de>
2023-09-15 11:06:18 +0200

    Fixed issue with pgfplots.

--- Project1/Plots/modify_plots.sh
+++ Project1/Plots/modify_plots.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# ensure the paths work
+sed -i 's/addplot graphics.\{0,300\}] {\.\.\/\.\.\/Plots\//addplot graphics$1] {/' *temp_state_10000.pgf
 sed -i 's/addplot graphics.\{0,300\}] {/&\.\.\/\.\.\/Plots\//' *temp_state_10000.pgf
+
+# remove the legends
+sed -i '/\\addlegend/d' Energies_maxwell_distribution_MC_step_fast_{0..3}.pgf
