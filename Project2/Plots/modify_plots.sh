4d0692de70cb3bca104df061a94a4bb445e5b61f
Theo <koppenth@hotmail.de>
2023-10-01 15:06:22 +0200

    Fixed LIF withdrawal.

--- Project2/Plots/modify_plots.sh
+++ Project2/Plots/modify_plots.sh
@@ -4,19 +4,19 @@
 # sed -i 's/addplot graphics.\{0,300\}] {\.\.\/\.\.\/Plots\//addplot graphics$1] {/' *temp_state_10000.pgf
 # sed -i 's/addplot graphics.\{0,300\}] {/&\.\.\/\.\.\/Plots\//' *temp_state_10000.pgf
 
-# remove the legends
-# sed -i '/\\addlegend/d' Energies_maxwell_distribution_MC_step_fast_*.pgf
+ALLPLOTS=$(ls {N,O,T,NO}_*.pgf LIF_withdrawal.pgf)
+RIGHTPLOTS=$(ls {O,T,NO}_*.pgf)
 
 # general style
-sed -i 's/\\begin{axis}\[/\\begin{axis}\[\n ytick={0,0.2,0.4,0.6,0.8,1},\n x tick label style = {rotate=70},\n y post scale=3, \n transpose legend,/' {N,O,T}_*.pgf
-sed -i 's/legend style={fill opacity=0.8, draw opacity=1, text opacity=1, draw=lightgray204},/legend style={fill opacity=0.8, draw opacity=1, text opacity=1, draw=lightgray204, anchor=south east, at={(0.97,0.03)}},/' {N,O,T}_*.pgf
-sed -i 's/at={(0.97,0.03)}/at={(axis cs:5,1.1)}/' {N,O,T}_*.pgf
+sed -i 's/\\begin{axis}\[/\\begin{axis}\[\n ytick={0,0.2,0.4,0.6,0.8,1},\n x tick label style = {rotate=70},\n y post scale=3, \n transpose legend,/' $ALLPLOTS
+sed -i 's/legend style={fill opacity=0.8, draw opacity=1, text opacity=1, draw=lightgray204},/legend style={fill opacity=0.8, draw opacity=1, text opacity=1, draw=lightgray204, anchor=south east, at={(0.97,0.03)}},/' $ALLPLOTS
+sed -i 's/at={(0.97,0.03)}/at={(axis cs:5,1.1)}/' $ALLPLOTS
 perl -i -pe 's/anchor=south east/anchor=south west,
     legend columns=4,
-    \/tikz\/every even column\/.append style={column sep=1.0cm},/' {N,O,T}_*.pgf
+    \/tikz\/every even column\/.append style={column sep=1.0cm},/' $ALLPLOTS
 
 # align the plots
-sed -i 's/\\begin{tikzpicture}/\\begin{tikzpicture}\[baseline\]/' {N,O,T}_*.pgf
+sed -i 's/\\begin{tikzpicture}/\\begin{tikzpicture}\[baseline\]/' $ALLPLOTS
 
 # add legend entries
 perl -i -pe 's/\\addlegendentry{Nanog overexpressed}/\\addlegendentry{Nanog overexpressed}
@@ -30,9 +30,12 @@ perl -i -pe 's/\\addlegendentry{Nanog overexpressed}/\\addlegendentry{Nanog over
 \\addlegendentry{LIF active}/' N_*.pgf
 
 # remove axis label for Oct4 and Tet1
-sed -i '/ylabel={Expression level},/d' {O,T}_*.pgf
-sed -i '/xlabel={Time},/d' {O,T}_*.pgf
+sed -i '/ylabel={Expression level},/d' $RIGHTPLOTS
+sed -i '/xlabel={Time},/d' $RIGHTPLOTS
 
 # remove legends for Oct4 and Tet1
-sed -i '/\\addlegend/d' {O,T}_*.pgf
+sed -i '/\\addlegend/d' $RIGHTPLOTS
+
+# fix the style for the LIF_withdrawal
+sed -i '/y post scale=3,/d' LIF_withdrawal.pgf
 
--- Project2/main.py
+++ Project2/main.py
@@ -36,6 +36,8 @@ expressions = ['N','O','T']
 
 Experiment_labels = [f'{expr}_{over_expression}' 
                         for expr in expressions for over_expression in over_expressions]
+# Add experiments to the list
+Experiment_labels += ['NO_0.1']
 LIF_expression = {f'{exp_label}':False for exp_label in Experiment_labels}
 
 params = {exp_label:params_Mef.copy() 
@@ -50,6 +52,11 @@ for expr in expressions:
             params[f'{expr}_{over_expression}']['LIF'] = 0.06
         params[exp_label][f'{expr}_over'] = over_expression
 
+# This adds plot b)
+params['NO_0.1']['O_over'] = 0.1
+params['NO_0.1']['N_over'] = 0.3
+LIF_expression['NO_0.1'] = True
+
 # experiment series
 param_lists = {f'{exp_label}':[params_Mef, params[exp_label], params_default if LIF_expression[exp_label] else params_Mef]
                 for exp_label in Experiment_labels}
@@ -58,6 +65,9 @@ param_lists = {f'{exp_label}':[params_Mef, params[exp_label], params_default if 
 NOT_labels = {'N':'Nanog', 'O':'Oct4', 'T':'Tet1'}
 colors = {'N':'blue', 'O':'green', 'T':'orange'}
 
+# Add the LIF withdrawal experiment (plot c)
+Experiment_labels += ['LIF_withdrawal']
+param_lists['LIF_withdrawal'] = [params_default, params_Mef]
 
 def rhs(y, params=params_Mef):
     # y is of the form (N, O, T)
@@ -73,43 +83,55 @@ def rhs(y, params=params_Mef):
                   params['O_over']+params['LIF']+params['p_O']*NT_KNT2/(1+NT_KNT2)*O_KO/(1+O_KO)-O,
                   params['T_over']+params['p_T']*NT_KNT2/(1+NT_KNT2)*O_KO/(1+O_KO)-T])
 
-running_time = 30
+running_time = 40
 
-def run_experiment_series(param_list):
+def run_experiment_series(param_list, y = np.zeros(shape=(3,1))):
     t = np.empty(0)
-    y = np.zeros(shape=(3,0))
     for param in param_list:
-        y0 = np.zeros(3) if not y.shape[1] else y[:,-1]
+        y0 = y[:,-1]
         t0 = 0 if not len(t) else t[-1]
         soln = sc.integrate.solve_ivp(lambda t,y: rhs(y, param), (t0,t0+running_time), y0, vectorised=True, max_step=0.1)
         t = np.append(t, soln.t)
         y = np.append(y, soln.y, axis=1)
-    return t, y
+    return t, y[:,1:]
 
 
 
 if __name__ == '__main__':
 
     for j, exp_label in enumerate(Experiment_labels):
+        run_length = len(param_lists[exp_label])
         fig, ax = pl.subplots()
-        ax.set_xlim((0,3*running_time))
+        ax.set_xlim((0,run_length*running_time))
         ax.set_ylim(0,1.05)
         # ax.axes.set_aspect(3)
         ax.set_yticks([0,1])
-        ax.set_xticks([i*running_time for i in range(4)], labels=['' for i in range(4)], minor=True)
-        ax.set_xticks([(i+0.5)*running_time for i in range(3)], labels=[f'${exp_label[0]}_\\text{{over}}\!=0$', f'${exp_label[0]}_\\text{{over}}\!={exp_label[2:]}$', f'${exp_label[0]}_\\text{{over}}\!=0$'])
-        t, y = run_experiment_series(param_lists[exp_label])
+        ax.set_xticks([i*running_time for i in range(run_length+1)], labels=['' for i in range(run_length+1)], minor=True)
+        # set x tick labels
+        if exp_label == 'LIF_withdrawal':
+            xlabels = ['$LIF=0.06$','$LIF=0$']
+        else:
+            xlabels = [f'${exp_label[0]}_\\text{{over}}\!=0$, $\\text{{LIF}}=0$', f'${exp_label[0]}_\\text{{over}}\!={exp_label[2:]}$, $\\text{{LIF}}=0.06$', f'${exp_label[0]}_\\text{{over}}\!=0$, $\\text{{LIF}}=0.06$'] \
+                            if LIF_expression[exp_label] else [f'${exp_label[0]}_\\text{{over}}\!=0$', f'${exp_label[0]}_\\text{{over}}\!={exp_label[2:]}$', f'${exp_label[0]}_\\text{{over}}\!=0$']
+
+
+        ax.set_xticks([(i+0.5)*running_time for i in range(run_length)], labels=xlabels)
+        y0 = 0.7*np.ones((3,1)) if exp_label == 'LIF_withdrawal' else np.zeros((3,1))
+        t, y = run_experiment_series(param_lists[exp_label], y0)
+
         for i, expr in enumerate(expressions):
-            pl.plot(t[::(1000//len(t))], y[i,::(1000//len(t))], label=NOT_labels[f'{expr}'], color=colors[f'{expr}'], linewidth=2)
+            pl.plot(t[::(1000//len(t)+1)], y[i,::(1000//len(t)+1)], label=NOT_labels[f'{expr}'], color=colors[f'{expr}'], linewidth=2)
 
 
         ax.set_xlabel('Time')
         ax.set_ylabel('Expression level')
         # add background for experiment
-        ax.axvspan(running_time, 2*running_time, alpha=0.2, color=colors[exp_label[0]], label=f'{NOT_labels[exp_label[0]]} overexpressed')
-        if LIF_expression[exp_label]:
-            ax.axvspan(running_time, 3*running_time, alpha=0.2, color='gray', label='LIF active')  
-            ax.set_xticks([(i+0.5)*running_time for i in range(3)], labels=[f'${exp_label[0]}_\\text{{over}}\!=0$, $\\text{{LIF}}=0$', f'${exp_label[0]}_\\text{{over}}\!={exp_label[2:]}$, $\\text{{LIF}}=0.06$', f'${exp_label[0]}_\\text{{over}}\!=0$, $\\text{{LIF}}=0.06$'])      
+        if exp_label == 'LIF_withdrawal':
+            ax.axvspan(0, running_time, alpha=0.2, color='gray', label='LIF active')  
+        else:
+            ax.axvspan(running_time, 2*running_time, alpha=0.2, color=colors[exp_label[0]], label=f'{NOT_labels[exp_label[0]]} overexpressed')
+            if LIF_expression[exp_label]:
+                ax.axvspan(running_time, 3*running_time, alpha=0.2, color='gray', label='LIF active')  
 
         # Add a legend
         pl.legend()
