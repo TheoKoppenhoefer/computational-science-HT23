0560c8077d130c2c959dc0fe2cbfca8bc0e82e6e
Theo <koppenth@hotmail.de>
2023-10-04 13:33:46 +0200

    Wrote some more on my report.

--- Project2/main.py
+++ Project2/main.py
@@ -8,7 +8,8 @@ from pgf_plot_fix import tikzplotlib_fix_ncols
 import matplotlib.ticker as ticker
 
 # TODO:
-# - add background to graphs to legend
+# - fix the line width
+# - implement modification
 # - implement the other graphs
 # - fix issue with >= 1
 
@@ -31,7 +32,7 @@ params_MEF = {
 # initialise experiments
 params_LIF = params_MEF.copy()
 params_LIF['LIF'] = 0.06
-over_expressions = [0.1, 0.13, 0.2, 0.3]
+over_expressions = [0.1, 0.13, 0.2, 0.3] # [0.1,0.3,0.8]
 expressions = ['N','O','T']
 
 Experiment_labels = [f'{expr}_{over_expression}' 
@@ -74,7 +75,7 @@ params_zero = params_MEF.copy()
 params_zero['T_over'] = 0
 param_lists['LIF_withdrawal_0'] = [params_LIF, params_zero]
 
-def rhs(y, params=params_MEF):
+def rhs(y, params=params_MEF, repression=False):
     # y is of the form (N, O, T)
     # params id of the form of default_params
     N = np.take(y,0)
@@ -84,18 +85,28 @@ def rhs(y, params=params_MEF):
     O_KO = O/params['K_O']
     NT = (params['K_d']+N+T)/2-np.sqrt(((params['K_d']+N+T)/2)**2-N*T)
     NT_KNT2 = (NT/params['K_NT'])**params['n']
-    return np.array([params['N_over']+params['LIF']+params['p_N']*O_KO/(1+O_KO)-N,
+    if not repression: return np.array([params['N_over']+params['LIF']+params['p_N']*O_KO/(1+O_KO)-N,
                   params['O_over']+params['LIF']+params['p_O']*NT_KNT2/(1+NT_KNT2)*O_KO/(1+O_KO)-O,
                   params['T_over']+params['p_T']*NT_KNT2/(1+NT_KNT2)*O_KO/(1+O_KO)-T])
+    U = np.take(y,3)
+    V = np.take(y,4)
+    U_KU = U/0.1
+    V_KV = V/0.1
+    O_Knew = O/0.1
+    return np.array([params['N_over']+params['LIF']+params['p_N']*O_KO/(1+O_KO)/(1+U_KU)/(1+V_KV)-N,
+                  params['O_over']+params['LIF']+params['p_O']*NT_KNT2/(1+NT_KNT2)*O_KO/(1+O_KO)-O,
+                  params['T_over']+params['p_T']*NT_KNT2/(1+NT_KNT2)*O_KO/(1+O_KO)-T,
+                  O_Knew/(1+O_Knew)-U,
+                  O_Knew/(1+O_Knew)-V])
 
 running_time = 40
 
-def run_experiment_series(param_list, y = np.zeros(shape=(3,1))):
+def run_experiment_series(param_list, y = np.zeros(shape=(3,1)), repression=False):
     t = np.empty(0)
     for param in param_list:
         y0 = y[:,-1]
         t0 = 0 if not len(t) else t[-1]
-        soln = sc.integrate.solve_ivp(lambda t,y: rhs(y, param), (t0,t0+running_time), y0, vectorised=True, max_step=0.1)
+        soln = sc.integrate.solve_ivp(lambda t,y: rhs(y, param, repression), (t0,t0+running_time), y0, vectorised=True, max_step=0.1)
         t = np.append(t, soln.t)
         y = np.append(y, soln.y, axis=1)
     return t, y[:,1:]
@@ -103,7 +114,8 @@ def run_experiment_series(param_list, y = np.zeros(shape=(3,1))):
 
 
 if __name__ == '__main__':
-    show_plots=False
+    show_plots=True
+    repression = False
 
     for j, exp_label in enumerate(Experiment_labels):
         run_length = len(param_lists[exp_label])
@@ -114,22 +126,13 @@ if __name__ == '__main__':
         ax.set_yticks([0,1])
         ax.set_xticks([i*running_time for i in range(run_length+1)], labels=['' for i in range(run_length+1)], minor=True)
         # set x tick labels
-        if exp_label == 'LIF_withdrawal':
-            xlabels = ['$\\text{{LIF}}=0.06$','$\\text{{LIF}}=0$']
-        elif exp_label == 'LIF_withdrawal_0':
-            xlabels = ['$\\text{{LIF}}=0.06$, $T_\\text{{over}}\!=0.05$','$\\text{{LIF}}=0$, $T_\\text{{over}}\!=0$']
-        elif exp_label == 'NO_0.1':
-            xlabels = ['$N_\\text{{over}}\!=0$, $O_\\text{{over}}\!=0$, $\\text{{LIF}}=0$', '$N_\\text{{over}}\!=0.3$, $O_\\text{{over}}\!=0.1$, ${{LIF}}=0.06$', '$N_\\text{{over}}\!=0$, $O_\\text{{over}}\!=0$, $\\text{{LIF}}=0.06$']
-        else:
-            xlabels = [f'${exp_label[0]}_\\text{{over}}\!=0$, $\\text{{LIF}}=0$', f'${exp_label[0]}_\\text{{over}}\!={exp_label[2:]}$, $\\text{{LIF}}=0.06$', f'${exp_label[0]}_\\text{{over}}\!=0$, $\\text{{LIF}}=0.06$'] \
-                            if LIF_expression[exp_label] else [f'${exp_label[0]}_\\text{{over}}\!=0$', f'${exp_label[0]}_\\text{{over}}\!={exp_label[2:]}$', f'${exp_label[0]}_\\text{{over}}\!=0$']
-        
-        if show_plots:
-            xlabels = [label.replace(r'\text','') for label in xlabels]
+        xlabels = [''.join([f'${k}={params[k]}$' if params[k]!=params_MEF[k] else '' for k in params_MEF]) for params in param_lists[exp_label]] 
+        xlabels = [label.replace('_over',r'_\text{over}').replace('LIF',r'\text{LIF}').replace('$$','$, $') for label in xlabels]
+        if show_plots: xlabels = [label.replace(r'\text','') for label in xlabels]
 
         ax.set_xticks([(i+0.5)*running_time for i in range(run_length)], labels=xlabels)
-        y0 = 0.7*np.ones((3,1)) if exp_label[:14] == 'LIF_withdrawal' else np.zeros((3,1))
-        t, y = run_experiment_series(param_lists[exp_label], y0)
+        y0 = 0.7*np.ones((5 if repression else 3,1)) if exp_label[:14] == 'LIF_withdrawal' else np.zeros((5 if repression else 3,1))
+        t, y = run_experiment_series(param_lists[exp_label], y0, repression)
 
         for i, expr in enumerate(expressions):
             ax.plot(t[::(1000//len(t)+1)], y[i,::(1000//len(t)+1)], label=NOT_labels[f'{expr}'], color=colors[f'{expr}'], linewidth=2)
@@ -149,7 +152,7 @@ if __name__ == '__main__':
         pl.legend()
         tikzplotlib_fix_ncols(fig)
 
-        tikzplotlib.save(f'Plots/{exp_label}.pgf')
+        if not show_plots: tikzplotlib.save(f'Plots/{exp_label}.pgf')
 
     if show_plots: pl.show()
         
